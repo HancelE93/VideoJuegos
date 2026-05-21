b0b657afecae8bd09ecbd00d3a8f1cfdb614fe9f
HancelE1993 <hancelxyz@gmail.com>
2026-05-21 01:49:39 -0500

    5 buscar Lista creada y funcionando

--- src/main/java/com/krakdev/jdbc/videojuegos/VideojuegoJdbc.java
+++ src/main/java/com/krakdev/jdbc/videojuegos/VideojuegoJdbc.java
@@ -2,7 +2,10 @@ package com.krakdev.jdbc.videojuegos;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -10,13 +13,12 @@ import org.apache.logging.log4j.Logger;
 import com.krakdev.jdbc.Conexion;
 import com.krakdev.videojuegos.entidades.Videojuego;
 
-
-
 public class VideojuegoJdbc {
 
-	
 	private static final Logger log = LogManager.getLogger(VideojuegoJdbc.class);
-	public static Videojuego insertar(String codigo, String nombre, String plataforma, double precio, boolean disponible, String genero) {
+
+	public static Videojuego insertar(String codigo, String nombre, String plataforma, double precio,
+			boolean disponible, String genero) {
 
 		Connection con = null;
 		PreparedStatement ps = null;
@@ -28,7 +30,7 @@ public class VideojuegoJdbc {
 			String sql = "insert into Videojuego (codigo, nombre, plataforma, precio, disponible, genero) values(?,?,?,?,?.?)";
 
 			ps = con.prepareStatement(sql);
-			
+
 			ps.setString(1, codigo);
 			ps.setString(2, nombre);
 			ps.setString(3, plataforma);
@@ -36,13 +38,13 @@ public class VideojuegoJdbc {
 			ps.setBoolean(5, disponible);
 			ps.setString(6, genero);
 
-			videoJuego = new Videojuego(codigo, nombre, plataforma, precio, disponible,genero);
+			videoJuego = new Videojuego(codigo, nombre, plataforma, precio, disponible, genero);
 			int filas = ps.executeUpdate();
-			log.info("Filas insertadas: " + filas);
+			log.info("Video juego agregado: " + filas);
 
 		} catch (Exception e) {
-			log.error("Error al insertar" + e.getMessage());
-			throw new RuntimeException("Error al insertar" + e.getMessage());
+			log.error("Error al insertar el video juegos" + e.getMessage());
+			throw new RuntimeException("Error al insertar video juegor" + e.getMessage());
 
 		} finally {
 			try {
@@ -54,4 +56,46 @@ public class VideojuegoJdbc {
 		}
 		return videoJuego;
 	}
+
+	public static List<Videojuego> lista() {
+
+		List<Videojuego> videoJuegos = new ArrayList<>();
+		Connection con = null;
+
+		try {
+			con = Conexion.getConnection();
+
+			String sql = "select * from Videojuegos";
+
+			PreparedStatement ps = con.prepareStatement(sql);
+
+			ResultSet rs = ps.executeQuery();
+
+			while (rs.next()) {
+
+				Videojuego vj = new Videojuego(rs.getString("codigo"), rs.getString("nombre"),
+						rs.getString("plataforma"), rs.getDouble("precio"), rs.getBoolean("disponible"),
+						rs.getString("genero"));
+
+				videoJuegos.add(vj);
+
+				log.info("Lista  de video juegos agregada");
+
+			}
+		} catch (Exception e) {
+			log.error("Error al buscar lista de video juegos" + e.getMessage());
+			throw new RuntimeException("Error al ver la lista de video juegos" + e.getMessage());
+		} finally {
+			try {
+				con.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return videoJuegos;
+	}
+
+	
+
 }
